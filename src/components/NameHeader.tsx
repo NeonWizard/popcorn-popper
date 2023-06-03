6eeeceb18bc3a0ba8dd09ba2f361b4120940534f
Wes <averagewizard13@gmail.com>
2023-06-02 19:05:30 -0700

    Add NameHeader component

---
+++ src/components/NameHeader.tsx
@@ -0,0 +1,37 @@
+import React, { FunctionComponent } from "react";
+
+import "./style.css";
+import styled from "styled-components";
+
+interface NameHeaderProps {
+  name: string;
+  role: string;
+}
+
+const Container = styled.div`
+  position: fixed;
+  top: 100px;
+  margin: 0;
+
+  h1 {
+    color: white;
+    margin: 0;
+  }
+  p {
+    color: #aaa;
+    margin: 0;
+  }
+`;
+
+const NameHeader: FunctionComponent<NameHeaderProps> = (
+  props: NameHeaderProps
+) => {
+  return (
+    <Container>
+      <h1>{props.name}</h1>
+      <p>{props.role}</p>
+    </Container>
+  );
+};
+
+export default NameHeader;
--- src/main/main.tsx
+++ src/main/main.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import * as Phys from "react-dom-box2d";
 
 import Nameball from "../components/Nameball";
+import NameHeader from "../components/NameHeader";
 
 import "./main.css";
 
@@ -16,11 +17,14 @@ const Main = () => {
   }, [memberList]);
 
   return (
-    <Phys.World width={600} height={400} gravity={[0, 9.8]} className="world">
-      {memberList.map((member) => (
-        <Nameball name={member} role="ui designer"></Nameball>
-      ))}
-    </Phys.World>
+    <>
+      <NameHeader name="Wes" role="Software Engineer"></NameHeader>
+      <Phys.World width={600} height={400} gravity={[0, 9.8]} className="world">
+        {memberList.map((member) => (
+          <Nameball name={member} role="ui designer"></Nameball>
+        ))}
+      </Phys.World>
+    </>
   );
 };
 
