7ccd5fc04f8394d6f00329ebd89843503f4a9bb1
Wes <averagewizard13@gmail.com>
2023-06-03 13:10:27 -0700

    Add outer PhysWorld

--- src/main/main.tsx
+++ src/main/main.tsx
@@ -18,8 +18,28 @@ const Main = () => {
 
   return (
     <>
+      <Phys.World
+        width={window.innerWidth}
+        height={window.innerHeight}
+        gravity={[0, 5]}
+        className="world-screen"
+        style={{ position: "fixed" }}
+      >
+        <Nameball
+          name="I'm freeee"
+          role="cya"
+          left={window.innerWidth / 2}
+        ></Nameball>
+      </Phys.World>
+
       <NameHeader name="Wes" role="Software Engineer"></NameHeader>
-      <Phys.World width={600} height={400} gravity={[0, 9.8]} className="world">
+      <Phys.World
+        width={600}
+        height={400}
+        gravity={[0, 9.8]}
+        className="world-box"
+        style={{ backgroundColor: "#111214" }}
+      >
         {memberList.map((member) => (
           <Nameball name={member} role="ui designer"></Nameball>
         ))}
