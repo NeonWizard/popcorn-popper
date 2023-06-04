a7c743f012cc1f9ee03dd51ee0a4d0e03b5b6437
Wes <averagewizard13@gmail.com>
2023-06-04 14:43:49 -0700

    Make Nameball style scoped

--- src/components/Nameball.tsx
+++ src/components/Nameball.tsx
@@ -5,6 +5,7 @@ import { Shake } from "reshake";
 import "./style.css";
 import CurvedText from "./CurvedText";
 import { useScreenShakeContext } from "../ScreenShakeContext";
+import styled from "styled-components";
 
 interface NameballProps {
   name: string;
@@ -15,35 +16,62 @@ interface NameballProps {
   textColor?: string;
 }
 
+const Style = styled.div`
+  .circle {
+    // background-color: #bb0000;
+    // background: radial-gradient(#d00, #900);
+    // background: radial-gradient(#22222a, #1a1a1f) !important;
+    box-shadow: 0px 0px 2px 2px rgba(0, 0, 0, 0.6);
+    text-align: center;
+    height: 120px;
+    width: 120px;
+    border: 1px solid black;
+    user-select: none;
+
+    font-family: monospace;
+    font-size: 1rem;
+    color: white;
+    text-transform: uppercase;
+  }
+
+  .circle:hover {
+    background-color: #fff !important;
+    color: black !important;
+    cursor: pointer;
+  }
+`;
+
 const Nameball: FunctionComponent<NameballProps> = (props: NameballProps) => {
   const { setScreenShake } = useScreenShakeContext();
   const diameter = 100;
 
   return (
     <Shake h={2} v={2} r={0} int={1}>
-      <Phys.Item
-        shape="circle"
-        left={props.left}
-        top={props.top}
-        restitution={0.65}
-        width={diameter}
-        height={diameter}
-      >
-        <div
-          className="circle1"
-          style={{ backgroundColor: props.fillColor, color: "white" }}
-          onClick={() => {
-            setScreenShake(true);
-            setTimeout(() => setScreenShake(false), 100);
-          }}
+      <Style>
+        <Phys.Item
+          shape="circle"
+          left={props.left}
+          top={props.top}
+          restitution={0.65}
+          width={diameter}
+          height={diameter}
         >
-          <CurvedText
-            upperText={props.name}
-            lowerText={props.role}
-            objectSize={diameter}
-          ></CurvedText>
-        </div>
-      </Phys.Item>
+          <div
+            className="circle"
+            style={{ backgroundColor: props.fillColor, color: "white" }}
+            onClick={() => {
+              setScreenShake(true);
+              setTimeout(() => setScreenShake(false), 100);
+            }}
+          >
+            <CurvedText
+              upperText={props.name}
+              lowerText={props.role}
+              objectSize={diameter}
+            ></CurvedText>
+          </div>
+        </Phys.Item>
+      </Style>
     </Shake>
   );
 };
